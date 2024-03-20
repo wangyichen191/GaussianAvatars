151d1bb8566175abd7b4cb5320322be974fe26db
wangyichen191 <wangyichen191@mails.ucas.ac.cn>
2024-03-20 12:33:01 +0000

    fix start compute offset in densify and prune

--- render.sh
+++ render.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python render.py -m output/UNION10EMOEXP_218_eval_600k --skip_test --skip_val --select_camera_id 6 --iteration 120000 --use_offset
+CUDA_VISIBLE_DEVICES=3 python render.py -m output/GaussianOffset_smallFLame_lowerRegular_302 --skip_test --skip_val --select_camera_id 6 --iteration 120000 --use_offset
--- train.sh
+++ train.sh
@@ -1,4 +1,7 @@
-CUDA_VISIBLE_DEVICES=3 ../anaconda3/envs/gaussian-avatars/bin/python3 train.py \
--s data/cluster/ikarus/sqian/project/dynamic-head-avatars/code/multi-view-head-tracker/export/UNION10_218_EMO1234EXP234589_v16_DS2-0.5x_lmkSTAR_teethV3_SMOOTH_offsetS_whiteBg_maskBelowLine \
--m output/GaussianOffset \
---port 60019 --eval --white_background --bind_to_mesh --not_finetune_flame_params 
+CUDA_VISIBLE_DEVICES=0 ../anaconda3/envs/gaussian-avatars/bin/python3 train.py \
+-s /mnt/sda/wangyichen/server15_2/GaussianAvatars/data/302/cluster/ikarus/sqian/project/dynamic-head-avatars/code/multi-view-head-tracker/export/UNION10_302_EMO1234EXP234589_v16_DS2-0.5x_lmkSTAR_teethV3_SMOOTH_offsetS_whiteBg_maskBelowLine \
+-m output/302_woFinetune \
+--port 60019 --eval --white_background --bind_to_mesh --not_finetune_flame_params --compute_offset_iteration -1 \
+--position_offset_regularization 0.2 \
+--rotation_offset_regularization 0.2 \
+--scaling_offset_regularization 0.2
