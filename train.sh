2f237edc6c738ddf6e60445abea940bffdacd77e
wangyichen191 <wangyichen191@mails.ucas.ac.cn>
2024-03-21 02:53:26 +0000

    nothing changed in train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 ../anaconda3/envs/gaussian-avatars/bin/python3 train.py \
--s /mnt/sda/wangyichen/server15_2/GaussianAvatars/data/302/cluster/ikarus/sqian/project/dynamic-head-avatars/code/multi-view-head-tracker/export/UNION10_302_EMO1234EXP234589_v16_DS2-0.5x_lmkSTAR_teethV3_SMOOTH_offsetS_whiteBg_maskBelowLine \
+-s /data2/wangyichen/GaussianAvatars/data/302/cluster/ikarus/sqian/project/dynamic-head-avatars/code/multi-view-head-tracker/export/UNION10_302_EMO1234EXP234589_v16_DS2-0.5x_lmkSTAR_teethV3_SMOOTH_offsetS_whiteBg_maskBelowLine \
 -m output/302_woFinetune \
 --port 60019 --eval --white_background --bind_to_mesh --not_finetune_flame_params --compute_offset_iteration -1 \
 --position_offset_regularization 0.2 \
